ba022d8270e47fdd88e4c35950ad5bce572c052b
noedge55 <noedge@yeah.net>
2017-08-18 17:38:29 +0800

    将留言列表显示出来并，添加回复功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,7 @@
 //获取应用实例
 var app = getApp()
 var Bmob = require('../../utils/bmob.js');
-var Common = require('../../utils/common.js');
+var common = require('../../utils/common.js');
 var that ;
 Page({
   data: {
@@ -17,9 +17,10 @@ Page({
     duration: 1000,
     imgList: app.globalData.imgList,
     leaveMsgList: [],
-    pageSize: 2,
-    limit : 2,
-    count : 0
+    pageSize: 5,
+    limit : 5,
+    count : 0,
+    inputContent:''
   },
   senLeaveMsg:function(e){
     if(e.detail.value.content == ""){
@@ -42,17 +43,23 @@ Page({
       leaveMsg.save(null, {
         success: function (result) {
           console.log("留言发布成功,objectId:" + result.id);
+          common.showModal("留言发布成功");
         },
         error: function (result, error) {
           console.log("留言发布失败");
           console.log(error);
         }
       });
+      this.setData({
+        inputContent:''
+      });
+      this.onShow();
     }
   },
   onLoad: function () {
     console.log('onLoad');
   },
+
   onShow: function () {
     var msgList = new Array();
 
@@ -71,6 +78,27 @@ Page({
         console.log("总数据量：",result);
       }
     });
+  },
+  onReachBottom:function(e){
+    var limit = that.data.limit;
+    console.log("上拉加载更多..." + that.data.limit);
+    if (that.data.limit > that.data.pageSize && that.data.limit - that.data.pageSize >= that.data.count) {
+      common.showModal("已经是最后一页");
+      return false;
+    }
+    that.setData({
+      limit : limit + that.data.pageSize
+    });
+    this.onShow();
+  },
+  onPullDownRefresh: function(){
+    wx.stopPullDownRefresh();
+    console.log("下拉刷新");
+    common.showModal("下拉刷新");
+    that.setData({
+      limit : that.data.pageSize
+    })
+    that.onShow();
   }
 })
 
@@ -81,4 +109,69 @@ function getReturn(){
     return false;
   }
 
+  var msgList = new Array();
+  wx.getStorage({
+    key: 'user_id',
+    success: function(res) {
+      if(res.data){
+        var LeaveMsg = Bmob.Object.extend("leave_message");
+        var query = new Bmob.Query(LeaveMsg);
+
+        if(that.data.limit >= that.data.pageSize){
+          query.limit(that.data.limit);
+        }
+
+        query.descending("createAt");
+        query.include("user");
+
+        query.find({
+          success: function(results){
+            for(var i = 0 ; i < results.length; i++){
+              var userId = results[i].get("user").id; //获取用户id
+              var content = results[i].get("content");//获取留言内容
+              var id = results[i].id;//获取留言id
+              var createAt = results[i].createdAt;//获取留言创建时间
+              var userPic = results[i].get("user").get("userPic");//获取用户头像
+              var likeNum = results[i].get("likenum");//获取留言被赞次数
+              var replyNum = results[i].get("replynum");//获取留言被回复次数
+
+              var isLike = false; 
+
+              var Lk = Bmob.Object.extend("is_like");
+              var query1 = new Bmob.Query(Lk);
+              query1.equalTo("liker",Bmob.User.current());
+              query1.equalTo("leavemsg",results[i]);
+              query1.select("islike");
+              query1.find().then(function(ress){
+                isLike = ress;
+              });
+
+              var jsonA;
+              jsonA = {
+                "userId"  : userId   || '',
+                "id"      : id       || '',
+                "content" : content  || '',
+                "createAt": createAt || '',
+                "userPic" : userPic  || '',
+                "likeNum" : likeNum  || 0 ,
+                "replyNum": replyNum || 0 ,
+                "isLike"  : isLike   || false
+              }
+              msgList.push(jsonA);
+            }
+            that.setData({
+              leaveMsgList : msgList
+            });
+          },
+          error : function(error){
+            common.dataLoading(error, "loading");
+            console.log(error);
+          }
+        });
+      }
+    },
+    fail: function(error){
+      console.log("查询留言列表失败" + error);
+    }
+  })
 }
--- pages/msgDetail/msgDetail.js
+++ pages/msgDetail/msgDetail.js
@@ -1,12 +1,65 @@
 // pages/msgDetail/msgDetail.js
 var app = getApp();
+var Bmob = require('../../utils/bmob.js');
+var common = require('../../utils/common.js');
 var that ;
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    item:"rabit"
+    itemId:'',
+    itemContent:'',
+    itemPic:'',
+    itemLikeNum:'',
+    itemReplyNum:'',
+
+    replyImgSrc:'/images/reply.png',
+    likeImgSrc:'/images/like.png',
+  },
+  changeReplyImg:function(e){
+    that = this;
+    that.setData({
+      replyImgSrc:'/images/reply_s.png'
+    });
+  },
+  resetReplyImg:function(e){
+    that = this;
+    that.setData({
+      replyImgSrc:'/images/reply.png'
+    });
+  },
+  sendReplyMsg:function(e){
+    that = this;
+    var LeaveMsg = Bmob.Object.extend("leave_message");
+    var query = new Bmob.Query(LeaveMsg);
+    query.get(e.target.dataset.id,{
+      success:function(result){
+        var ReplyMsg = Bmob.Object.extend("reply_message");
+        var replyMsg = new ReplyMsg();
+        replyMsg.set("user",Bmob.User.current());
+        replyMsg.set("leavemsg",result);
+        replyMsg.set("content", e.detail.value.content);
+        replyMsg.save(null,{
+          success:function(result2){
+            common.showModal("回复成功");
+            console.log(result2);
+            //修改留言中的回复数量
+            result.set('replynum',result.get('replynum')+1);
+            result.save();
+            that.setData({
+              itemReplyNum: result.get('replynum')
+            });
+          },
+          error:function(e){
+            console.log(e);
+          }
+        })
+      }
+    });
+  },
+  changeLike:function(e){
+    
   },
 
   /**
@@ -15,7 +68,11 @@ Page({
   onLoad: function (options) {
     that = this;
     that.setData({
-      item:options.item
+      itemId:options.itemId,
+      itemContent:options.itemContent,
+      itemPic:options.itemPic,
+      itemLikeNum:options.itemLikeNum,
+      itemReplyNum:options.itemReplyNum
     })
   },
 
