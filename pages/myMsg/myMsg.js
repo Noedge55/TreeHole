38dac993e94abe58450bf6ae5436963bf84ff071
noedge55 <noedge@yeah.net>
2017-09-08 22:21:10 +0800

    完善我的留言与回复

--- pages/myMsg/myMsg.js
+++ pages/myMsg/myMsg.js
@@ -35,6 +35,9 @@ Page({
   onShow: function () {
     let that = this;
     if(that.data.display === "none"){
+      wx.setNavigationBarTitle({
+        title: '我的留言',
+      })
       let LeaveMsg = Bmob.Object.extend("leave_message");
       let query = new Bmob.Query(LeaveMsg);
       query.equalTo("user",Bmob.User.current());
@@ -59,38 +62,42 @@ Page({
         }
       })
     }else{
+      wx.setNavigationBarTitle({
+        title: '我的回复',
+      })
       let ReplyMsg = Bmob.Object.extend("reply_message");
       let query = new Bmob.Query(ReplyMsg);
       query.equalTo("user",Bmob.User.current());
-      query.ascending("id");
+      query.ascending("leavemsg");
       query.find({
         success:function(results){
-          let rmId = '';
+          var rmId = '';
           var list = new Array();
           for(let i = 0 ; i < results.length ; i++){
-            if(results[i].get("leavemsg").id != rmId){
-              rmId = results[i].get("leavemsg").id;
-              //查询该条回复对应的留言
-              let LeaveMsg = Bmob.Object.extend("leave_message");
-              let query1 = new Bmob.Query(LeaveMsg);
-              query1.get(rmId,{
-                success:function(result){
-                  let jsonA = {
-                    rmId: results[i].get("leavemsg").id,//获取回复id
-                    lmId: results[i].id,//获取留言id
-                    rmContent: results[i].get("content"),//获取回复内容
-                    lmContent: result.get("content")//获取对应留言内容
-                  }
-                  list.push(jsonA);
-                  that.setData({
-                    lmList: list
-                  });
-                },
-                error:function(error){
-                  console.log(error);
-                }
-              })
+            if(results[i].get("leavemsg").id == rmId){
+              continue;
             }
+            rmId = results[i].get("leavemsg").id;
+            //查询该条回复对应的留言
+            let LeaveMsg = Bmob.Object.extend("leave_message");
+            let query1 = new Bmob.Query(LeaveMsg);
+            query1.get(results[i].get("leavemsg").id, {
+              success: function (result) {
+                let jsonA = {
+                  rmId: results[i].get("leavemsg").id,//获取回复id
+                  lmId: results[i].id,//获取留言id
+                  rmContent: results[i].get("content"),//获取回复内容
+                  lmContent: result.get("content")//获取对应留言内容
+                }
+                list.push(jsonA);
+                that.setData({
+                  lmList: list
+                });
+              },
+              error: function (error) {
+                console.log(error);
+              }
+            })
           }
         },
         error:function(error){
